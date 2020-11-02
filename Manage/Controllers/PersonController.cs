63575e0e928ebbd656e42cbc6e0e4e65dfb470cb
Vitalii <vpempus@gmail.com>
2020-11-02 14:04:33 +0200

    Added my list from db to view

--- Manage/Controllers/PersonController.cs
+++ Manage/Controllers/PersonController.cs
@@ -2,8 +2,12 @@
 using ContactManager.DAL.Repository;
 using ContactManager.Models;
 using ContactManager.Models.DTO;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace Manage.Controllers
 {
@@ -26,7 +30,79 @@ namespace Manage.Controllers
         {
             var people = _repository.GetAll();
             return Ok(_mapper.Map<List<PersonDTO>>(people));
-
+        }
+        [HttpGet, Route("{id}", Name = "DisplayRoute")]
+        [ResponseType(typeof(PersonDTO))]
+        public async Task<IHttpActionResult> GetOne(int id)
+        {
+            Person item = _repository.GetOne(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<PersonDTO>(item));
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        [HttpPost, Route("")]
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult PostInventory(Person item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                _repository.Add(item);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return CreatedAtRoute("DisplayRoute", new { id = item.Id }, item);
+        }
+        [HttpDelete, Route("{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                _repository.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return Ok();
+        }
+        [HttpPut, Route("{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutChange(int id, Person item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _repository.Save(item);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return StatusCode(HttpStatusCode.NoContent);
         }
     }
 }
